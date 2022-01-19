6c6765ad476d130977668ee4ede9edc48e53b491
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2022-01-19 16:00:47 -0300

    Agregué persistencia de datos, el array de las tareas por hacer se guardan en la carpeta DB, en el archivo data.json

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const argv = require("./config/yargs").argv;
+const porHacer = require("./por-hacer/por-hacer");
 
 let comando = argv._[0];
 
 switch (comando) {
     case "crear":
-        console.log("Crear una tarea por hacer.");
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
     case "listar":
         console.log("Mostrar tareas por hacer.");
